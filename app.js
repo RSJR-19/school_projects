134a3e299e90c4e6ce59954998684be5d92ef5b8
RSJR-19 <sumbadrenan619@gmail.com>
2025-09-26 12:49:46 +0800

    school_projects

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+const name = process.argv[2];
+
+if (name) {
+    console.log(`Hello ${name}! Welcome to Node.js.`);
+}else {
+    console.log("Hello World, Pass your name like: node app.js Renan");
+    }
